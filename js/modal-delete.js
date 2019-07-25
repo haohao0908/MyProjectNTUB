9ed1787d72f976449f15855294bf6db0d3f28bdb
haohao0908 <N1056444@ntub.edu.tw>
2019-07-25 22:40:06 +0800

    修正

--- js/modal-delete.js
+++ js/modal-delete.js
@@ -4,12 +4,14 @@ $(document).ready(function(){
     $('.change-card-deletes').on('click',function(){
         $('.change-card-deletes-check').show();
         $('.btn-delete-close').show();
+        $('.change-card-deletes').hide();
         $('.change-card-deletes-check').on('click',function(){
             $('#card-1').remove();
         });   
         $('.btn-delete-close').on('click',function(){
             $('.change-card-deletes-check').hide();
             $('.btn-delete-close').hide();
+            $('.change-card-deletes').show();
         });
     });
 });
