0bcb8f9f18effef972007f6e52ca9268d68fe18c
haohao0908 <N1056444@ntub.edu.tw>
2019-07-26 12:00:40 +0800

    修正

--- js/Sortable.js
+++ js/Sortable.js
@@ -10,25 +10,20 @@ $(function () {
         axis: "x", items: ".work-total",cancel:".dropright"});
     $("#work-total-move").disableSelection();
     //卡片互相移動
-    $(".work-card").on("taphold",function(){
-        $("#work-body-move-1,#work-body-move-2").sortable({
-            connectWith: "#work-body-move-1,#work-body-move-2",items:'.work-card'});
-        $("#work-body-move-1,#work-body-move-2").disableSelection();
-      });
-    // $("#work-body-move-1,#work-body-move-2").sortable({
-    //     connectWith: "#work-body-move-1,#work-body-move-2",items:'.work-card'});
-    // $("#work-body-move-1,#work-body-move-2").disableSelection();
+    $("#work-body-move-1,#work-body-move-2").sortable({
+        connectWith: "#work-body-move-1,#work-body-move-2",items:'.work-card',distance: 15});
+    $("#work-body-move-1,#work-body-move-2").disableSelection();
     //input、textarea可以點選拖曳也可以編輯
-    $('input').on('click', function(e) { 
-        $(this).trigger({ 
-         type: 'mousedown', 
-         which: 3 
-        }); 
-    }); 
+    // $('input').on('click', function(e) { 
+    //     $(this).trigger({ 
+    //      type: 'mousedown', 
+    //      which: 3 
+    //     }); 
+    // }); 
     $('input').on('mousedown', function(e) { 
-        if(e.which == 3){ 
+        // if(e.which == 3){ 
          $(this).focus(); 
-        } 
+        // } 
     }); 
     $('textarea').on('mousedown', function(e) { 
          $(this).focus(); 
