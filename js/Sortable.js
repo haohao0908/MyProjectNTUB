207d11e7381c73cef50f6d96339702d23fc4728b
haohao0908 <N1056444@ntub.edu.tw>
2019-07-26 16:52:28 +0800

    修改

--- js/Sortable.js
+++ js/Sortable.js
@@ -11,27 +11,21 @@ $(function () {
     $("#work-total-move").disableSelection();
     //卡片互相移動
     $("#work-body-move-1,#work-body-move-2").sortable({
-        connectWith: "#work-body-move-1,#work-body-move-2",items:'.work-card'});
+        connectWith: "#work-body-move-1,#work-body-move-2",items:'.work-card',distance: 15});
     $("#work-body-move-1,#work-body-move-2").disableSelection();
     //input、textarea可以點選拖曳也可以編輯
-    $("input").on("click", function (){
-        $(this).focus();
-    })
-    $("textarea").on("click", function (){
-        $(this).focus();
-    })
     // $('input').on('click', function(e) { 
     //     $(this).trigger({ 
     //      type: 'mousedown', 
     //      which: 3 
     //     }); 
     // }); 
-    // $('input').on('mousedown', function(e) { 
-    //     if(e.which == 3){ 
-    //      $(this).focus(); 
-    //     } 
-    // }); 
-    // $('textarea').on('mousedown', function(e) { 
-    //      $(this).focus(); 
-    // }); 
+    $('input').on('mousedown', function(e) { 
+        // if(e.which == 3){ 
+         $(this).focus(); 
+        // } 
+    }); 
+    $('textarea').on('mousedown', function(e) { 
+         $(this).focus(); 
+    }); 
 });
