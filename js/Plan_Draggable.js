8b7941682d68d9a111693ae2fda7a5460daf6c27
haohao0908 <N1056444@ntub.edu.tw>
2019-08-01 13:46:37 +0800

    修正

--- js/Plan_Draggable.js
+++ js/Plan_Draggable.js
@@ -1,57 +1,58 @@
-
+var elements = document.getElementsByClassName("work-card");
+for (var i = 0; i < elements.length; i++) {
+    elements[i].ontouchstart = function () {
+        clearTimeout(this.downTimer);
+        this.downTimer = setTimeout(function () {
+            $("#work-body-move-1,#work-body-move-2").sortable({
+                disabled: false,
+                connectWith: "#work-body-move-1,#work-body-move-2"
+            });
+            $("#work-body-move-1,#work-body-move-2").disableSelection();
+        }, 1000)
+    };
+    elements[0].ontouchend = function () {
+        clearTimeout(this.downTimer);
+        $("#work-body-move-1,#work-body-move-2").sortable({ disabled: true });
+    };
+}
 
 $(document).ready(function () {
     $(window).width();
-    var mouseEventTypes = {
-        touchstart: "mousedown",
-        touchmove: "mousemove",
-        touchend: "mouseup"
-    };
-
-    for (originalType in mouseEventTypes) {
-        document.addEventListener(originalType, function (originalEvent) {
-            event = document.createEvent("MouseEvents");
-            touch = originalEvent.changedTouches[0];
-            event.initMouseEvent(mouseEventTypes[originalEvent.type], true, true,
-                window, 0, touch.screenX, touch.screenY, touch.clientX,
-                touch.clientY, touch.ctrlKey, touch.altKey, touch.shiftKey,
-                touch.metaKey, 0, null);
-            originalEvent.target.dispatchEvent(event);
-        });
-    }
     //成員管理者移動
     //列表移動
     $("#admin-move,#member-move").sortable({
         axis: "x", connectWith: "#admin-move,#member-move", opacity: 0.7, items: ".member-img-trash"
     });
     $("#admin-move,#member-move").disableSelection();
-    if ($(window).width() < 1024) {
-        $('.work-card').mousedown(function (event) {
-            clearTimeout(this.downTimer);
-            this.downTimer = setTimeout(function () {
-                $("#work-body-move-1,#work-body-move-2").sortable({
-                    disabled: false,
-                    connectWith: "#work-body-move-1,#work-body-move-2"
-                });
-                $("#work-body-move-1,#work-body-move-2").disableSelection();
-            }, 1000);
-        }).mouseup(function (e) {
-            clearTimeout(this.downTimer);
-            $("#work-body-move-1,#work-body-move-2").sortable({ disabled: true });
-        });
-    }
-    else {
-        $("#work-body-move-1,#work-body-move-2").sortable({
-            connectWith: "#work-body-move-1,#work-body-move-2"
-        });
+    // if ($(window).width() < 1024) {
+    //     $('.work-card').mousedown(function (event) {
+    //         clearTimeout(this.downTimer);
+    //         this.downTimer = setTimeout(function () {
+    //             alert('a')
+    //             $("#work-body-move-1,#work-body-move-2").sortable({
+    //                 disabled: false,
+    //                 connectWith: "#work-body-move-1,#work-body-move-2"
+    //             });
+    //             $("#work-body-move-1,#work-body-move-2").disableSelection();
+    //         }, 1000);
+    //     }).mouseup(function (e) {
+    //         clearTimeout(this.downTimer);
+    //         alert('b')
+    //         $("#work-body-move-1,#work-body-move-2").sortable({ disabled: true });
+    //     });
+    // }
+    // else {
+    //     $("#work-body-move-1,#work-body-move-2").sortable({
+    //         connectWith: "#work-body-move-1,#work-body-move-2"
+    //     });
 
-        $("#work-body-move-1,#work-body-move-2").disableSelection();
+    //     $("#work-body-move-1,#work-body-move-2").disableSelection();
 
-        $("#work-total-move").sortable({
-            axis: "x", items: ".work-total", cancel: ".drpdown"
-        });
-        $("#work-total-move").disableSelection();
-    }
+    //     $("#work-total-move").sortable({
+    //         axis: "x", items: ".work-total", cancel: ".drpdown"
+    //     });
+    //     $("#work-total-move").disableSelection();
+    // }
 
     // $("#work-body-move-1,#work-body-move-2").sortable({
     //     connectWith: "#work-body-move-1,#work-body-move-2"
