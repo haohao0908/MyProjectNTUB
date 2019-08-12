3acd8eeb45ba05a33a8cb29845636ca54ed9c0c8
haohao0908 <n1056444@ntub.edu.tw>
2019-08-12 20:17:15 +0800

    修正拖曳點擊

--- js/Plan_CardLabelToggle.js
+++ js/Plan_CardLabelToggle.js
@@ -9,9 +9,18 @@ $(document).ready(function () {
             $('.card-label-2').animate({ lineHeight: '20px' });
             $('.card-label-3').animate({ height: '20px' });
             $('.card-label-3').animate({ lineHeight: '20px' });
+            $('.card-label-4').animate({ height: '20px' });
+            $('.card-label-4').animate({ lineHeight: '20px' });
+            $('.card-label-5').animate({ height: '20px' });
+            $('.card-label-5').animate({ lineHeight: '20px' });
+            $('.card-label-6').animate({ height: '20px' });
+            $('.card-label-6').animate({ lineHeight: '20px' });
             $(".card-label-1").css("max-width","100%");
             $(".card-label-2").css("max-width","100%");
             $(".card-label-3").css("max-width","100%");
+            $(".card-label-4").css("max-width","100%");
+            $(".card-label-5").css("max-width","100%");
+            $(".card-label-6").css("max-width","100%");
             isTrue=false;
         }
         else{
@@ -21,9 +30,18 @@ $(document).ready(function () {
             $('.card-label-2').animate({ lineHeight: '100px' });
             $('.card-label-3').animate({ height: '8px' });
             $('.card-label-3').animate({ lineHeight: '100px' });
+            $('.card-label-4').animate({ height: '8px' });
+            $('.card-label-4').animate({ lineHeight: '100px' });
+            $('.card-label-5').animate({ height: '8px' });
+            $('.card-label-5').animate({ lineHeight: '100px' });
+            $('.card-label-6').animate({ height: '8px' });
+            $('.card-label-6').animate({ lineHeight: '100px' });
             $(".card-label-1").css("max-width","45px");
             $(".card-label-2").css("max-width","45px");
             $(".card-label-3").css("max-width","45px");
+            $(".card-label-4").css("max-width","45px");
+            $(".card-label-5").css("max-width","45px");
+            $(".card-label-6").css("max-width","45px");
             isTrue=true;
         }
     });
--- js/Plan_Draggable.js
+++ js/Plan_Draggable.js
@@ -65,4 +65,3 @@ function browserRedirect() {
 }
 
 browserRedirect();
-
